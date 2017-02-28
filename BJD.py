8f3b4ebbe0dd787e54cb5ade506946b43c51001a
Jeremy <jeremyd@The-Cathode-Ray-Mission.local>
2017-02-28 09:49:23 -0500

    add extract function

--- BJD.py
+++ BJD.py
@@ -28,41 +28,40 @@ def is_upper(datanode):
 def is_lower(datanode):
     return not is_upper(datanode)
 
-G=ip.G
- 
-m_list= [n[0] for n in G.nodes(data=True) if is_lower(n)]#list of men
 
-f_list= [n[0] for n in G.nodes(data=True) if is_upper(n)]#list of women
+def extract(G):
+    """Returns BJD of the graph G, or an error if graph is not bipartite""" 
+    m_list= [n[0] for n in G.nodes(data=True) if is_lower(n)]#list of men
+
+    f_list= [n[0] for n in G.nodes(data=True) if is_upper(n)]#list of women
 
 
-km=max([G.degree(i) for i in m_list])
-kw=max([G.degree(i) for i in f_list])
+    km=max([G.degree(i) for i in m_list])
+    kw=max([G.degree(i) for i in f_list])
 
-N=[[]]
-dN=[[]]
-dd_list=np.zeros(shape=(km,kw))
-for i in range(0,km):
-    degi=[]
-    for j in m_list:
-        if G.degree(j)==i+1:
-            degi.append(j)
-    Nj=[]
-    dNj=[]
-    for j in degi:
-        Nj.extend(G.neighbors(j))
-                
-    for n in Nj:
-        dNj.append(G.degree(n))
-                    
-    N.append(Nj) 
-    dN.append(dNj)
-            
-    for jj in range(0,kw):
-        dd_list[i][jj]=(dN[i+1]).count(jj+1)
+    N=[[]]
+    dN=[[]]
+    dd_list=np.zeros(shape=(km,kw))
+    for i in range(0,km):
+        degi=[]
+        for j in m_list:
+            if G.degree(j)==i+1:
+                degi.append(j)
+        Nj=[]
+        dNj=[]
+        for j in degi:
+            Nj.extend(G.neighbors(j))
 
-         
-                         
-Bjd=dd_list.astype(int)   
-BJD=Bjd.transpose()
+        for n in Nj:
+            dNj.append(G.degree(n))
 
-return BJD
+        N.append(Nj) 
+        dN.append(dNj)
+
+        for jj in range(0,kw):
+            dd_list[i][jj]=(dN[i+1]).count(jj+1)
+
+    Bjd=dd_list.astype(int)   
+    BJD=Bjd.transpose()
+
+    return BJD

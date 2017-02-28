ca3760de71f4322d5ec3b2bf2362e83cf2d20444
azizi8612036 <toobasemani@gmail.com>
2017-02-28 14:30:10 -0600

    fix net
    find fixed sub net

--- test_script.py
+++ test_script.py
@@ -1,11 +1,14 @@
 import BJD
 import networkx as nx
 import numpy as np
- 
+import Fix
 G = nx.read_gml('data/test5000.gml') 
+#G = nx.read_gml('data/small_test.gml') 
 ddtable = BJD.extract(G)
 ddtable = np.array(ddtable)
 
-print(np.transpose(ddtable))
-print(BJD.validate(ddtable))
+#print(np.transpose(ddtable))
+#print(BJD.validate(ddtable))
+M=Fix.subA(G)
+print M.size()
 

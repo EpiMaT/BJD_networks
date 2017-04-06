0388c6f244c7ab99fc971baad6b76edb2678ff03
jdewar <jdewar@tulane.edu>
2017-04-06 11:24:24 -0400

    minor refactor; need to solve python 2vs3 use more elegantly

--- BJD.py
+++ BJD.py
@@ -1,13 +1,6 @@
 import numpy as np
 import handle_funcs as HF
 
-try:
-    xrange
-except NameError:
-    print("Running in Python 3")
-    xrange = range
-else:
-    print("Running in Python 2")
 
 _node = 0
 _data = 1
@@ -15,10 +8,8 @@ _data = 1
 _upper = 0
 _lower = 1
 
-
-
 def extract(G):
-    """Returns BJD of the graph G, or an error if graph is not bipartite""" 
+    """Returns BJD of the graph G, or an error if graph is not bipartite"""
     m_list= [n[0] for n in G.nodes(data=True) if HF.is_lower(n)]#list of men
 
     f_list= [n[0] for n in G.nodes(data=True) if HF.is_upper(n)]#list of women
@@ -43,14 +34,13 @@ def extract(G):
         for n in Nj:
             dNj.append(G.degree(n))
 
-        N.append(Nj) 
+        N.append(Nj)
         dN.append(dNj)
 
         for jj in range(0,kw):
             dd_list[i][jj]=(dN[i+1]).count(jj+1)
 
-    Bjd=dd_list.astype(int)   
+    Bjd=dd_list.astype(int)
     BJD=Bjd.transpose()
 
     return BJD
-

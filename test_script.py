f223bb39985bb12fef05e8bf799c29e37001cd3b
azizi8612036 <toobasemani@gmail.com>
2017-05-01 11:44:53 -0500

    revise code after removing extra files

--- test_script.py
+++ test_script.py
@@ -5,13 +5,13 @@ import graphs as g
 import Fix
 import Generate_Network 
 import handle_funcs as HF
-import Reduced_BJD as RBJD
+
 reload(HF)
-reload(RBJD)
+
 reload(Fix)
 reload(BJD)
 reload(Generate_Network)
-G = nx.read_gml('data/test5000.gml') 
+
 G = nx.read_gml('data/small_test.gml') 
 ddtable = BJD.extract(G)
 ddtable = np.array(ddtable)
@@ -24,4 +24,8 @@ B=Generate_Network.make_graph(nx.Graph(),g.small,'random_edge')
 print('original net', B.edges())
 S=Fix.sub(B)
 print('sub net', S.edges())
-print('matrix',RBJD.RBJD(B,S))
+
+for i in B.nodes():
+    for j in B.neighbors(i):
+        print(len(nx.shortest_path(B,source=i,target=j))-1)
+        print(len(nx.shortest_path(B,source=j,target=i))-1)
